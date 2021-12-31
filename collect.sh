19d5981d0f3e87acbfa093125fcd005d6d1c526d
cN3rd <subs.in.tokyo@gmail.com>
2021-12-31 16:03:17 +0200

    build: fix deps so they work with vanilla 3.6.5

--- collect.sh
+++ collect.sh
@@ -1,3 +1,6 @@
+# build dependencies
+echo "creating install directory..."
+rm -rf install
 mkdir -p install
 cp -r build/zlib/install/* install
 cp -r build/minizip/install/* install
@@ -11,4 +14,21 @@ cp -r build/curl/install/* install
 cp -r build/freetype/install/* install
 cp -r build/nghttp2/install/* install
 cp -r build/glew/install/* install
+
+# move freetype 2 headers back a little
+echo "fixing freetype includes..."
+mv install/include/freetype2/* install/include
+rm -rf install/include/freetype2
+
+# rename zlib to match
+echo "fixing zlib library name..."
+mv install/lib/zlibstaticd.lib install/lib/zlibd.lib
+mv install/lib/zlibstatic.lib install/lib/zlib.lib
+
+# rename libpng artifacts
+echo "fixing libpng library name..."
+mv install/lib/libpng16_staticd.lib install/lib/libpng16d.lib
+mv install/lib/libpng16_static.lib install/lib/libpng16.lib
+
+# build package
 tar -zcvf install.tar.gz install
